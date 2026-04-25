648ead3a990a6c33c55e5a5501388325fe533fc7
denis defolie <denis.defolie@miraceti.net>
2026-04-25 22:15:06 +0200

    adminer

--- test_tube_scanner/scanner/views.py
+++ test_tube_scanner/scanner/views.py
@@ -54,7 +54,7 @@ def reductstore_view(request):
 
 @login_required
 def adminer_view(request):
-    return render(request, "scanner/iframe.html", context=global_context(request, link=f'http://{settings.DOMAIN_SERVER}:8181/'))
+    return render(request, "scanner/iframe.html", context=global_context(request, link=f'http://{settings.DOMAIN_SERVER}:81/'))
 
 @login_required
 def portainer_view(request):
